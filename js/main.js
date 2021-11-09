f0267680b5f1418cf4631350d4c849d931b60483
Ostap Blystsiv <ostapcr7@gmail.com>
2021-11-09 21:07:58 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn')
+const theme = document.getElementById('theme')
+const defTheme = document.getElementById('def-theme')
+
+btn.addEventListener("click", () => {
+
+});
